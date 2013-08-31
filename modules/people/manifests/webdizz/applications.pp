a0d39415c15edca91a98c3d2a6bf50a37a878b77
Izzet Mustsfaiev <webdizz@gmail.com>
2013-08-31 16:13:12 +0300

    teamocil was added to be installed

--- modules/people/manifests/webdizz/applications.pp
+++ modules/people/manifests/webdizz/applications.pp
@@ -1,16 +1,29 @@
 class people::webdizz::applications {
+    require ruby
 
-  include skype
-  include vagrant
-  include virtualbox
-  include intellij
-  include chrome
-  include wget
-  include dropbox
-  include cyberduck
-  include mou
-  include googledrive
-  include shortcat
-  include emacs
+    include skype
+    include vagrant
+    include virtualbox
+    include intellij
+    include chrome
+    include wget
+    include dropbox
+    include cyberduck
+    include mou
+    include googledrive
+    include shortcat
+    include emacs
+
+    package { 'tmux':
+        ensure => installed,
+    }
+
+    class { 'ruby::global':
+        version => '1.8.7'
+    }
+    ruby::gem { "teamocil":
+        gem     => 'teamocil',
+        ruby    => $version
+    }
 
 }
