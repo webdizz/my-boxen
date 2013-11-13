894c599f593b4f00ef2374dff0e4af90c6db1776
Izzet Mustsfaiev <webdizz@gmail.com>
2013-11-13 23:47:19 +0200

    Update boxen so that it works on Mavericks

--- modules/people/manifests/webdizz.pp
+++ modules/people/manifests/webdizz.pp
@@ -1,18 +1,18 @@
 class people::webdizz {
 
-    $home     = "/Users/${::boxen_user}"
-    $dev       = "${home}/dev"
+  $home     = "/Users/${::boxen_user}"
+  $dev       = "${home}/dev"
 
-    file { $dev:
-        ensure  => directory
-    }
+  file { $dev:
+    ensure  => directory
+  }
 
-    include people::webdizz::tools
-    include java
-    include people::webdizz::applications
-    include people::webdizz::dotfiles
-    include people::webdizz::private_dotfiles
-    include people::webdizz::os::preferences
+  include people::webdizz::tools
+  include java
+  include people::webdizz::applications
+  include people::webdizz::dotfiles
+  include people::webdizz::private_dotfiles
+  #include people::webdizz::os::preferences
 
 
 }
--- modules/people/manifests/webdizz/tools/ruby.pp
+++ modules/people/manifests/webdizz/tools/ruby.pp
@@ -1,19 +1,19 @@
 class people::webdizz::tools::ruby {
 
-  include ruby::2_0_0
-  include ruby::1_9_3
+  include ruby::2_0_0_p247
+  include ruby::1_9_3_p448
 
-  $ruby_version = '2.0.0'
+  $ruby200 = '2.0.0-p247'
 
   ruby::gem { "teamocil":
     gem     => 'teamocil',
     ruby    => $ruby_version
   }
 
-  $version = "1.9.3"
+  $ruby193 = '1.9.3-p448'
   ruby::gem { "buildr for ${version}":
     gem     => 'buildr',
-    ruby    => $version,
+    ruby    => $ruby193,
     version => '= 1.4.14'
   }
 
