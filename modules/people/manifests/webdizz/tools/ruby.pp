dd235a2e0278aa73d692366b488905af8f287af8
Izzet Mustsfaiev <webdizz@gmail.com>
2014-06-26 16:59:42 +0300

    Update Vagrant and Ruby

--- modules/people/manifests/webdizz/tools/ruby.pp
+++ modules/people/manifests/webdizz/tools/ruby.pp
@@ -1,35 +1,24 @@
 class people::webdizz::tools::ruby {
 
-  $ruby2 = '2.0.0-p353'
+  $ruby2 = '2.0.0-p481'
   ruby::version { $ruby2:}
 
-  $ruby193 = '1.9.3-p484'
+  $ruby193 = '1.9.3-p547'
   ruby::version { $ruby193: }
 
-  ruby::gem { "teamocil":
+  ruby_gem { "teamocil":
     gem     => 'teamocil',
-    ruby    => $ruby2
+    ruby_version    => $ruby2,
   }
 
-  ruby::gem { "teamocil193":
+  ruby_gem { "teamocil193":
     gem     => 'teamocil',
-    ruby    => $ruby193
+    ruby_version    => $ruby193,
   }
 
-  ruby::gem { "buildr for ${version}":
+  ruby_gem { "buildr for ${version}":
     gem     => 'buildr',
-    ruby    => $ruby193,
-    version => '= 1.4.14'
-  }
-
-  # Installing rbenv plugin
-  ruby::plugin  { 'rbenv-vars':
-    ensure => 'v1.2.0',
-    source  => 'sstephenson/rbenv-vars'
-  }
-
-  ruby::plugin  { 'rbenv-bundle-exec':
-    ensure => 'master',
-    source  => 'maljub01/rbenv-bundle-exec'
+    ruby_version    => $ruby193,
+    version=> '= 1.4.17',
   }
 }
