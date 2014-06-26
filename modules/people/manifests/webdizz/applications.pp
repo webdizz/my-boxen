ad08b6a5058bb9f74be9bdc950e9dd7621661d6e
Izzet Mustsfaiev <webdizz@gmail.com>
2014-06-26 17:34:04 +0300

    Add vagrant vm fusion license

--- modules/people/manifests/webdizz/applications.pp
+++ modules/people/manifests/webdizz/applications.pp
@@ -50,5 +50,7 @@ class people::webdizz::applications {
 
   vagrant::plugin { 'vagrant-omnibus':}
   vagrant::plugin { 'vagrant-berkshelf':}
-  vagrant::plugin { 'vagrant-vmware-fusion':}
+  vagrant::plugin { 'vagrant-vmware-fusion':
+    license => 'file:///Users/webdizz/Documents/vagrant_license.lic'
+  }
 }
