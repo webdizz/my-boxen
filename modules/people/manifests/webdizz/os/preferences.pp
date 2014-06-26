169cf228bafeb47ac74bc7a9f7b0df5d65cec8a2
Izzet Mustsfaiev <webdizz@gmail.com>
2014-06-26 15:11:14 +0300

    Remove custom fonst, will use special module for that

--- modules/people/manifests/webdizz/os/preferences.pp
+++ modules/people/manifests/webdizz/os/preferences.pp
@@ -1,19 +1,5 @@
 class people::webdizz::os::preferences {
 
-  #Install fonts
-  exec { 'Download Droid Fonts':
-    command => 'curl -L http://download.damieng.com/fonts/redistributed/DroidFamily.zip -o /tmp/droid-family.zip && mkdir /tmp/droid-family',
-    creates => '/tmp/droid-family.zip',
-    unless => "test -f /Users/${::boxen_user}/Library/Fonts/DroidSerif.ttf"
-  }
-
-  exec { 'Install Droid Fonts':
-    cwd => '/tmp/droid-family',
-    command => "unzip /tmp/droid-family.zip && cp /tmp/droid-family/Droid*.ttf /Users/${::boxen_user}/Library/Fonts/ && rm -rf /tmp/droid-*",
-    creates => "/Users/${::boxen_user}/Library/Fonts/DroidSerif.ttf",
-    onlyif => 'test -f /tmp/droid-family.zip'
-  }
-
   ########################
   ## basic#  system settings
   # ########################
