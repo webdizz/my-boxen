7518532a45b3f32822d31a6b4fee2067e9111d74
Izzet Mustsfaiev <webdizz@gmail.com>
2014-07-30 17:40:37 +0300

    Add GO installation

--- modules/people/manifests/webdizz/tools.pp
+++ modules/people/manifests/webdizz/tools.pp
@@ -3,4 +3,7 @@ class people::webdizz::tools {
     include people::webdizz::tools::ruby
     include people::webdizz::tools::java
     include people::webdizz::tools::gvm
+    include go
+    go::version { '1.1.1': }
+
 }
