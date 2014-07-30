108e21763549bbcd27273790ac0e87b7d3fd1384
Izzet Mustsfaiev <webdizz@gmail.com>
2014-07-30 17:30:36 +0300

    Update Kdiff3

--- modules/people/manifests/webdizz/applications/kdiff3.pp
+++ modules/people/manifests/webdizz/applications/kdiff3.pp
@@ -1,7 +1,7 @@
 class people::webdizz::applications::kdiff3 {
 
-  package { 'kdiff3':
-    provider   => 'appdmg',
-    source => 'http://kent.dl.sourceforge.net/project/kdiff3/kdiff3/0.9.97/kdiff3_0.9.97_MacOS_64bit.dmg'
+  package { 'Kdiff3':
+    provider   => 'pkgdmg',
+    source => 'http://cznic.dl.sourceforge.net/project/kdiff3/kdiff3/0.9.98/kdiff3-0.9.98-MacOSX-64Bit.dmg'
   }
 }
