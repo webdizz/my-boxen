f19e06637a360bc95fb1e2b780ff1cd230a442a6
Izzet Mustsfaiev <webdizz@gmail.com>
2014-07-31 13:10:23 +0300

    Fix Vim installation

--- modules/people/manifests/webdizz/applications/vim.pp
+++ modules/people/manifests/webdizz/applications/vim.pp
@@ -1,20 +1,12 @@
 class people::webdizz::applications::vim {
 
-    include vim
+  include vim
+  include vim::pathogen
 
-    vim::bundle { [
-        'scrooloose/syntastic',
-        'sjl/gundo.vim',
-        'tpope/vim-sensible',
-        'tpope/vim-endwise',
-        'tpope/vim-fugitive',
-        'tpope/vim-markdown',
-        'tpope/vim-tbone',
-        'tpope/vim-obsession',
-        'tpope/vim-commentary',
-        'tpope/vim-surround',
-        'altercation/vim-colors-solarized',
-        'promisedlandt/vim-colors-ir_black'
-    ]: }
+  $home = "/Users/${::boxen_user}"
 
+  repository { "${home}/.vim/bundle/Vundle.vim":
+    source => 'gmarik/Vundle.vim',
+    require => File["${home}/.vim"]
+  }
 }
