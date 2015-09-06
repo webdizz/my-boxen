f2791ceef839b77bbd0c2194ae92a680f29ac7aa
Izzet Mustsfaiev <webdizz@gmail.com>
2015-09-06 14:04:52 +0300

    Fix broken KDiff3 installation

--- modules/people/manifests/webdizz/applications/kdiff3.pp
+++ modules/people/manifests/webdizz/applications/kdiff3.pp
@@ -2,6 +2,6 @@ class people::webdizz::applications::kdiff3 {
 
   package { 'Kdiff3':
     provider   => 'pkgdmg',
-    source => 'http://cznic.dl.sourceforge.net/project/kdiff3/kdiff3/0.9.98/kdiff3-0.9.98-MacOSX-64Bit.dmg'
+    source => 'http://kent.dl.sourceforge.net/project/kdiff3/kdiff3/0.9.98/kdiff3-0.9.98-MacOSX-64Bit.dmg'
   }
 }
