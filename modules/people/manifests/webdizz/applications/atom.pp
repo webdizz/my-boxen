9f43001b5a97eaf0b2fd036f55ead8db5e769bb9
Izzet Mustsfaiev <webdizz@gmail.com>
2015-09-06 15:28:02 +0300

    Add packages for Atom

--- modules/people/manifests/webdizz/applications/atom.pp
+++ modules/people/manifests/webdizz/applications/atom.pp
@@ -7,5 +7,46 @@ class people::webdizz::applications::atom {
     atom::package { 'linter': }
     atom::package { 'build': }
     atom::package { 'sublime-style-column-selection': }
+    atom::package { 'highlight-column': }
+    atom::package { 'language-asciidoc': }
+    atom::package { 'language-docker': }
+    atom::package { 'pretty-json': }
+
+    atom::package { 'language-plantuml': }
+    atom::package { 'plantuml': }
+
+    # Puppet
+    atom::package { 'language-puppet': }
+    atom::package { 'linter-puppet': }
+
+    # Groovy
+    atom::package { 'atom-grails': }
+    atom::package { 'language-gradle': }
+    atom::package { 'language-groovy': }
+
+    # Ruby
+    atom::package { 'autocomplete-ruby': }
+
+    # Elixir
+    atom::package { 'autocomplete-elixir': }
+    atom::package { 'elixir-cmd': }
+    atom::package { 'erl-indent': }
+    atom::package { 'language-elixir': }
+    atom::package { 'linter-elixirc': }
+
+    # Go
+    atom::package { 'go-plus': }
+
+    # Java
+    atom::package { 'java-importer': }
+    atom::package { 'linter-javac': }
+
+    # Rust
+    atom::package { 'language-rust': }
+    atom::package { 'linter-rust': }
+    atom::package { 'rust-api-docs-helper': }
+
+    # Clojure
+    atom::package { 'linter-clojure': }
 
 }
