91300c515f79f9e5008a4ef63d76b39c416c48c7
Izzet Mustsfaiev <webdizz@gmail.com>
2015-09-06 15:12:20 +0300

    Change installation of smcFanControl to use brewcask

--- modules/people/manifests/webdizz/tools.pp
+++ modules/people/manifests/webdizz/tools.pp
@@ -3,18 +3,24 @@ class people::webdizz::tools {
   include people::webdizz::tools::gvm
   include people::webdizz::tools::vagrants
   include people::webdizz::tools::scala
+  include brewcask
 
   class {'packer':
     version => '0.8.6'
   }
 
-  class smcFanControl {
-    package { 'smcFanControl':
-      provider => 'compressed_app',
-      source   => 'http://www.eidac.de/smcfancontrol/smcfancontrol_2_4.zip'
-    }
+  package { 'smcfancontrol':
+    ensure => '2.5.2',
+    provider => 'brewcask'
   }
-  include smcFanControl
+  # class smcFanControl {
+  #   package { 'smcFanControl':
+  #     provider => 'compressed_app',
+  #     source   => 'http://www.eidac.de/smcfancontrol/smcfancontrol_2_5_2.zip',
+  #     ensure => '2.5.2'
+  #   }
+  # }
+  # include smcFanControl
 
   class visualVm {
     package { 'VisualVM':
