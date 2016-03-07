70861afc734d8810b86fb2f5859c95e75b50cc56
Izzet Mustafaiev <webdizz@gmail.com>
2016-03-07 12:02:38 +0200

    Upgrade Vagrant to 1.8.1

--- modules/people/manifests/webdizz/tools/vagrants.pp
+++ modules/people/manifests/webdizz/tools/vagrants.pp
@@ -1,8 +1,11 @@
 class people::webdizz::tools::vagrants {
 
-  class { 'vagrant': }
+  class { 'vagrant':
+    version => '1.8.1'
+  }
 
   vagrant::plugin { 'vagrant-omnibus':}
   vagrant::plugin { 'vagrant-berkshelf':}
   vagrant::plugin { 'vagrant-hosts':}
+  vagrant::plugin { 'landrush':}
 }
