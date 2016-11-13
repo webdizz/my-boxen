87abf23f486b38380f06a843f20cd0966386661d
Izzet Mustafayev <izzet_mustafayev@epam.com>
2016-11-13 16:54:29 +0200

    Update ruby version

--- modules/people/manifests/webdizz/langs/ruby.pp
+++ modules/people/manifests/webdizz/langs/ruby.pp
@@ -1,23 +1,20 @@
 class people::webdizz::langs::ruby {
 
-  $ruby2 = '2.0.0'
+  $ruby2 = '2.3.0'
   ruby::version { $ruby2:}
-
-  $ruby21 = '2.1.2'
-  ruby::version { $ruby21:}
 
   ruby_gem { "teamocil":
     gem     => 'teamocil',
     ruby_version    => $ruby2,
   }
 
-  ruby_gem { "teamocil21":
-    gem     => 'teamocil',
-    ruby_version    => $ruby21,
-  }
-
   ruby_gem { "travis":
     gem     => 'travis',
+    ruby_version    => $ruby2,
+  }
+  
+  ruby_gem { "rsense":
+    gem     => 'rsense',
     ruby_version    => $ruby2,
   }
 }
