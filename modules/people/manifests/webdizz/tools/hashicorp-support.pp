e8de2abb2ed25ae0690593046474e523e83837a9
Izzet Mustafayev <izzet_mustafayev@epam.com>
2016-12-29 16:00:14 +0200

    Update Vagrant

--- modules/people/manifests/webdizz/tools/hashicorp-support.pp
+++ modules/people/manifests/webdizz/tools/hashicorp-support.pp
@@ -3,9 +3,9 @@ class people::webdizz::tools::hashicorp-support {
   class {'packer':
     version => '0.12.0'
   }
-  
+
   class { 'vagrant':
-    version => '1.8.7'
+    version => '1.9.1'
   }
 
   vagrant::plugin { 'vagrant-omnibus':}
